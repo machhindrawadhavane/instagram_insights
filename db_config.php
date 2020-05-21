196e2dce2e26c71c001931e178443ac239eafd5a
amol <amol.chorghe@thenewj.com>
2020-05-21 08:46:18 +0530

    db user changes

--- db_config.php
+++ db_config.php
@@ -1,11 +1,19 @@
 <?php
- define('DB_SERVER', "mysqlservernewjprod.mysql.database.azure.com");
- define('DB_USER', "phantom@mysqlservernewjprod");
- define('DB_PASS', "Zurich$1");
- define('DB_DATABASE', "instagram_insights");
+// define('DB_SERVER', "mysqlservernewjprod.mysql.database.azure.com");
+// define('DB_USER', "phantom@mysqlservernewjprod");
+// define('DB_PASS', "Zurich$1");
+// define('DB_DATABASE', "instagram_insights");
+define('DB_SERVER', "mysqlservernewjprod.mysql.database.azure.com");
+define('DB_USER', "insta_cron");
+define('DB_PASS', "PassWord09");
+define('DB_DATABASE', "instagram_insights");
+
+
+/*define('DB_SERVER', "localhost");
+define('DB_USER', "root");
+define('DB_PASS', "");
+define('DB_DATABASE', "instagram_insights");*/
+?>
+
+
  
- /*define('DB_SERVER', "localhost");
- define('DB_USER', "root");
- define('DB_PASS', "");
- define('DB_DATABASE', "instagram_insights");*/
- ?>
